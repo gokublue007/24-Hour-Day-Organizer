cf626e1b92a8b8dc38498876fd647e90bacfc8fb
Elias Vasquez <eliasvasquez7@gmail.com>
2022-03-25 00:11:04 -0600

    Added time to page

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -1,15 +1,22 @@
 // Select everything
-const dateElement = document.querySelector('#currentDay');
-const saveElements = document.querySelector('.saveBtn');
+var timeDisplayEl = document.querySelector('#currentDay');
+var saveElements = document.querySelector('.saveBtn');
 
 // select all content fields
-const nineAM = document.querySelector('#9am');
-const tenAM = document.querySelector('#10am');
-const elevenAM = document.querySelector('#11am');
-const twelvePM = document.querySelector('#12pm');
-const onePM = document.querySelector('#1pm');
-const twoPM = document.querySelector('#2pm');
-const threePM = document.querySelector('#3pm');
-const fourPM = document.querySelector('#4pm');
-const fivePM = document.querySelector('#5pm');
+// var nineAM = document.querySelector('#9am');
+// var tenAM = document.querySelector('#10am');
+// var elevenAM = document.querySelector('#11am');
+// var twelvePM = document.querySelector('#12pm');
+// var onePM = document.querySelector('#1pm');
+// var twoPM = document.querySelector('#2pm');
+// var threePM = document.querySelector('#3pm');
+// var fourPM = document.querySelector('#4pm');
+// var fivePM = document.querySelector('#5pm');
+
+function displayTime() {
+    var rightNow = moment().format("MMM Do YYY, h:mm:ss a");
+    timeDisplayEl.textContent = rightNow
+};
+
+setInterval(displayTime, 1000);
 
