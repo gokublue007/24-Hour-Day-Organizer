582c477f035a8bc640d36f60d488da13195b521c
Elias Vasquez <eliasvasquez7@gmail.com>
2022-03-24 23:16:06 -0600

    Added save button

---
+++ Assest/js/script.js
@@ -0,0 +1,15 @@
+// Select everything
+const dateElement = document.querySelector('#currentDay');
+const saveElements = document.querySelector('.saveBtn');
+
+// select all content fields
+const nineAM = document.querySelector('#9am');
+const tenAM = document.querySelector('#10am');
+const elevenAM = document.querySelector('#11am');
+const twelvePM = document.querySelector('#12pm');
+const onePM = document.querySelector('#1pm');
+const twoPM = document.querySelector('#2pm');
+const threePM = document.querySelector('#3pm');
+const fourPM = document.querySelector('#4pm');
+const fivePM = document.querySelector('#5pm');
+
