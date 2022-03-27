5ea0a4410c99582b4760e4af24045da060b9cd70
Elias Vasquez <eliasvasquez7@gmail.com>
2022-03-27 16:13:25 -0600

    fixed time on var

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -6,8 +6,18 @@ var presentTime;
 var allHours = {
     morning: ["12:00AM, 1:00AM, 2:00AM, 3:00AM, 4:00AM, 5:00AM, 6:00AM, 7:00AM, 8:00AM"],
     work: ["9:00AM, 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"],
-    night: ["9:00AM, 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"]
+    night: ["6:00PM, 7:00PM, 8:00PM, 9:00PM, 10:00PM, 11:00PM"]
 };
+// select all content fields
+// var nineAM = document.getElementById("9am");
+// var tenAM = document.getElementById('10am');
+// var elevenAM = document.getElementById('11am');
+// var twelvePM = document.getElementById('12pm');
+// var onePM = document.getElementById('1pm');
+// var twoPM = document.getElementById('2pm');
+// var threePM = document.getElementById('3pm');
+// var fourPM = document.getElementById('4pm');
+// var fivePM = document.getElementById('5pm');
 
 function displayTime() {
     var rightNow = moment().format("MMM Do YYYY, h:mm:ss a");
@@ -20,26 +30,17 @@ changeColor();
 function changeColor() {
     presentTime = moment().format("hhA")
     console.log(presentTime)
-    if (allHours.morning.indexOf(presentTime) === -1 || allHours.night.indexOf(presentTime) !== - 1) {
-        $("#").css("background-color", "#d3d3d3");
+    if (allHours.morning.indexOf(presentTime) !== -1 || allHours.night.indexOf(presentTime) !== -1) {
+        $(".description").css("background-color", "#d3d3d3");
     } 
     for (var i = 0; i < allHours.work.indexOf(presentTime); i++){
         $("#" + allHours.work[i]).css("background-color", "#ACA1A1");
-    } for (var i = 0; i < allHours.work.indexOf(presentTime); i++){
-        $("#" + allHours.work[i]).css("background-color", "#77dd77"); i--;
+    } for (var i = 0; i < allHours.work.indexOf(presentTime); i--){
+        $("#" + allHours.work[i]).css("background-color", "#77dd77");
         }
 };
 
-// select all content fields
-// var nineAM = document.getElementById("9am");
-// var tenAM = document.getElementById('10am');
-// var elevenAM = document.getElementById('11am');
-// var twelvePM = document.getElementById('12pm');
-// var onePM = document.getElementById('1pm');
-// var twoPM = document.getElementById('2pm');
-// var threePM = document.getElementById('3pm');
-// var fourPM = document.getElementById('4pm');
-// var fivePM = document.getElementById('5pm');
+
 
 
 
