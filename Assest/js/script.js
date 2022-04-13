14db6f335f09c0aba95b1a67c6d90ac18792c6cf
Elias Vasquez <eliasvasquez7@gmail.com>
2022-04-13 14:26:18 +0800

    Made adjustments to make day Planner work

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -3,6 +3,7 @@ var timeDisplayEl = document.querySelector('#currentDay');
 var saveElements = document.querySelector('.saveBtn');
 var textArea = document.querySelectorAll('.description');
 var bussinessHours = [$("#9"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16"), $("#17")]
+var militryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 //set time for top of the page and time for blocks
 function displayTime() {
@@ -13,7 +14,7 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 presentTime = moment().hour();
-console.log(presentTime)
+// console.log(presentTime)
 
 //Added function to change block colors if past present or future. I couldnt figure what I was doing wrong to get the loop going correctly
 
@@ -21,12 +22,13 @@ function changeColor() {
     for (i = 0; i < bussinessHours.length; i++) {
         var timeEl = bussinessHours[i].attr("id");
         bussinessHours[i].children("textarea").val(JSON.parse(localStorage.getItem(timeEl)))
-        if (timeEl > presentTime) {
-            bussinessHours[i].children("textarea").addClass("past");
+        console.log(militryTime[i], presentTime)
+        if (militryTime[i] > presentTime) {
+            bussinessHours[i].children("textarea").addClass("future");
             console.log("past")
         }
-        if (timeEl < presentTime) {
-            bussinessHours[i].children("textarea").addClass("future");
+        if (militryTime[i] < presentTime) {
+            bussinessHours[i].children("textarea").addClass("past");
             console.log("futrue")
         } else {
             bussinessHours[i].children("textarea").addClass("present");
